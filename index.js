7623d488e1d04702084b2beb3ff0f5886d2eb921
bangbang93 <bangbang93@163.com>
2017-07-11 11:41:19 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by bangbang93 on 2017/7/11.
+ */
+'use strict';
+
+exports.SingleMessage = require('./vanilla/getui/message/SingleMessage');
+exports.Target = require('./vanilla/getui/Target');
+
+const GeTui = require('./vanilla/GT.push');
+
+console.log(GeTui.prototype);
